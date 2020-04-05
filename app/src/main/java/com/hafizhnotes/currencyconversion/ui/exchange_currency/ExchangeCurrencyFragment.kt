d426493b165fcfc750eed9fa94f64e335ad83850
ditohafizh <ditohafizh@gmail.com>
2020-04-06 00:54:29 +0700

    👔 design custom dropdown for currency form

--- app/src/main/java/com/hafizhnotes/currencyconversion/ui/exchange_currency/ExchangeCurrencyFragment.kt
+++ app/src/main/java/com/hafizhnotes/currencyconversion/ui/exchange_currency/ExchangeCurrencyFragment.kt
@@ -1,12 +1,15 @@
 package com.hafizhnotes.currencyconversion.ui.exchange_currency
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
 import com.hafizhnotes.currencyconversion.R
+import kotlinx.android.synthetic.main.fragment_exchange_currency.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +21,27 @@ class ExchangeCurrencyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exchange_currency, container, false)
+        val rootView =
+            inflater.inflate(R.layout.fragment_exchange_currency, container, false)
+
+        onBindStart(rootView)
+        return rootView
     }
 
+    private fun onBindStart(view: View) {
+        // Exit if activity is null.
+        if (activity == null) return
+
+        // Testing.
+        // Spinner.
+        val dataAdapter =
+            ArrayAdapter(
+                activity!!.applicationContext,
+                R.layout.spinner_common_text,
+                listOf("USD", "IDR")
+            )
+
+        dataAdapter.setDropDownViewResource(R.layout.spinner_common_dropdown)
+        view.sp_currency_from.adapter = dataAdapter
+    }
 }
